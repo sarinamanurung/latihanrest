2676924e9c2a9d9bc0503b5dc3527a43cc32efb3
sarina <sarinahmanurung57@gmail.com>
2019-07-05 09:38:05 +0700

    first commit

---
+++ src/main/java/com/eksad/latihanrest/LatihanrestApplication.java
@@ -0,0 +1,13 @@
+package com.eksad.latihanrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LatihanrestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LatihanrestApplication.class, args);
+	}
+
+}
